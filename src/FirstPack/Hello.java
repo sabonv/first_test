cfd31af53a75424f3b38ed7290c142e533c55ff4
sabonv <znakomiymne@list.ru>
2016-07-18 20:22:46 +0300

    Start first project in GIT

---
+++ src/FirstPack/Hello.java
@@ -0,0 +1,12 @@
+package FirstPack;
+
+/**
+ * Created by sabo on 18/07/16.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GIT");
+    }
+
+}
