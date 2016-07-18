99577f31c3f0e6bd57026fc9e5b2a0e2f3e7e148
sabonv <znakomiymne@list.ru>
2016-07-18 22:31:48 +0300

    Commit from IDEA

--- src/FirstPack/Hello.java
+++ src/FirstPack/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("hello again");
-
+        System.err.println("vot eto povorot");
     }
 
 }
