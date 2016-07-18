79810992e042639fe081d170a398684f44791215
sabonv <znakomiymne@list.ru>
2016-07-18 21:39:26 +0300

    Commit from IDEA (reverted from commit 27f7fc64ec39fe9903da14031fd6005f282355e9)

--- src/FirstPack/Hello.java
+++ src/FirstPack/Hello.java
@@ -8,7 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("hello again");
-        System.out.println("AGA BLYA - Check from IDEA");
 
     }
 
